ea6e689943cf47b64ab3024298ddf1390d5aa622
Gustavo Santiago <egustavo4@gmail.com>
2023-09-27 15:53:07 -0300

    remove accelpay logo

--- src/components/AccelpayNotice.jsx
+++ src/components/AccelpayNotice.jsx
@@ -1,18 +1,23 @@
-import '../styles/AccelpayNotice.scss';
+import "../styles/AccelpayNotice.scss";
 import { AccelpayLogo } from "./AccelpayLogo";
 
 export const AccelpayNotice = () => {
   return (
-    <a class="accelpay-notice-container" href={`https://accelpay.io?campaign=notice-rollout&referrer=${window.location.host}`} target="_blank">
+    <a
+      class="accelpay-notice-container"
+      href={`https://accelpay.io?campaign=notice-rollout&referrer=${window.location.host}`}
+      target="_blank"
+    >
       <span class="accelpay-text">
         All alcoholic beverage sales are made by licensed retailers in the
+        AccelPay network
       </span>
 
-      <span class="logo-span">
+      {/* <span class="logo-span">
         <AccelpayLogo />
-      </span>
+      </span> */}
 
-      <span class="accelpay-text">network</span>
+      {/* <span class="accelpay-text">network</span> */}
     </a>
   );
 };
