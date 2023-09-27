ec5978a96188bd4fdddaeace08aaf2e463c50b07
Gustavo Santiago <egustavo4@gmail.com>
2023-09-27 19:15:55 -0300

    add accelpay notice widget interval support

--- src/main.jsx
+++ src/main.jsx
@@ -2,30 +2,46 @@ import { render } from "preact";
 import { App } from "./app.jsx";
 import "./index.css";
 
+const renderTargetSelectors = (targetSelectors) => {
+  if (targetSelectors) {
+    for (const targetSelector of targetSelectors) {
+      if (targetSelector) {
+        const target = document.querySelector(targetSelector.selector);
+
+        if (target && !target.querySelector(".accelpay-widget-container")) {
+          console.log("Accelpay Notice Rendered!");
+          const container = document.createElement("div");
+          container.setAttribute(
+            "class",
+            `accelpay-widget-container ${targetSelector.extraClasses}`
+          );
+          target.appendChild(container);
+          render(<App />, container);
+        }
+      }
+    }
+  }
+};
+
 try {
-  const accelpayWidgetData = JSON.parse(
-    document.querySelector("#AccelpayWidgetData").textContent
-  );
-  const targetSelectors = accelpayWidgetData.accelpayWidgetTargets;
+  window.accelpayNoticeWidget = {};
+  const accelpayNoticeWidgetEl = document.querySelector("#AccelpayWidgetData");
+  if (accelpayNoticeWidgetEl) {
+    const accelpayNoticeWidgetDataStr = accelpayNoticeWidgetEl.textContent;
+    const accelpayNoticeWidgetData = JSON.parse(accelpayNoticeWidgetDataStr);
+    const targetSelectors = accelpayNoticeWidgetData.accelpayWidgetTargets;
+    const intervalTime = accelpayNoticeWidgetData.intervalTime || 2000;
 
-  for (const targetSelector of targetSelectors) {
-    const target = document.querySelector(targetSelector);
-
-    if (target) {
-      const container = document.createElement("div");
-      container.classList.add("accelpay-widget-container");
-      target.appendChild(container);
-      render(<App />, container);
+    if (targetSelectors) {
+      renderTargetSelectors(targetSelectors);
+      const targetsInterval = setInterval(() => renderTargetSelectors(targetSelectors), intervalTime);
+      window.accelpayNoticeWidget.clearTargetsInterval = () => clearInterval(targetsInterval);
     }
   }
 
-  window.accelpayNoticeWidget = {
-    renderNoticeComponent: (targetSelector) => {
-      const target = document.querySelector(targetSelector);
-      if (target) {
-        render(<App />, target);
-      }
-    },
+  window.accelpayNoticeWidget.renderNoticeComponent = (targetSelector) => {
+    const target = document.querySelector(targetSelector);
+    if (target) render(<App />, target);
   };
 
   console.log("Accelpay initialized!");
